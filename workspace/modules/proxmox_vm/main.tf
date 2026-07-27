b1702b764adeaa4140495892312c80afa03766a7
Rashun Baucum <baucumrashun@protonmail.com>
2026-07-26 21:51:05 -0400

    save state, break out nfs to server/client role, plus lvm role.

--- workspace/modules/proxmox_vm/main.tf
+++ workspace/modules/proxmox_vm/main.tf
@@ -51,16 +51,6 @@ resource "proxmox_virtual_environment_vm" "vms" {
     full  = false
   }
 
-    dynamic "ip_config" {
-      for_each = var.ipv4_config
-      content {
-        var.ipv4_key {
-          address = var.ipv4
-          gateway = var.gateway
-        }
-      }
-    }
-
     dynamic "disk" {
     for_each = var.additional_disks
     content {
