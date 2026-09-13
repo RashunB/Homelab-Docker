6b1cc4423c13f02580aa7288f26a7e07de0b996a
Rashun Baucum <baucumrashun@protonmail.com>
2026-09-13 14:38:35 -0400

    Updating path references to have path.module anchorings

--- workspace/deployments/media/application/main.tf
+++ workspace/deployments/media/application/main.tf
@@ -1,5 +1,5 @@
 data "sops_file" "media_platform" {
-  source_file = "../../../../secrets/media_platform.sops.yaml"
+  source_file = "${path.module}/../../../../secrets/media_platform.sops.yaml"
 }
 
 locals {
